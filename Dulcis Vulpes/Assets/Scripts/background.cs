9151add2fb919bf05e8c2f3d2f908c8d311787ec
alperentnc <alprn_08_@hotmail.com>
2023-01-22 16:54:02 +0300

    background color change through time

--- Dulcis Vulpes/Assets/Scripts/background.cs
+++ Dulcis Vulpes/Assets/Scripts/background.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class background : MonoBehaviour
 {
     public Transform cameraTransform;
-    // Start is called before the first frame update
+    SpriteRenderer spriteRendererToChangeColor;
+    Color colorr;
+    float colorCounter;
+     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRendererToChangeColor = GetComponent<SpriteRenderer>();
+        colorCounter = 255;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (colorCounter >= 0)
+        {
+            colorCounter -= Time.deltaTime * 2;
+            spriteRendererToChangeColor.color = (Color)(new Color32(255, (byte)colorCounter, (byte)colorCounter, 255));
+        }
+        else
+        {
+            colorr = new Color(255, 0, 0, 255);
+            spriteRendererToChangeColor.color = (Color)(new Color32(255, 0, 0, 255));
+        }
     }
     private void FixedUpdate()
     {
         transform.position = new Vector3(cameraTransform.position.x, 0.5f, -9);
+        
     }
 }
